e4b484d813cac101050df163e4b1de2e050e1b2a
ivan <ivanmg10@outlook.es>
2022-12-05 18:52:30 +0100

    contactos

--- src/pages/contacto/Contacto.js
+++ src/pages/contacto/Contacto.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import './contacto.css'
 
 const Contacto = () => {
-    return <h1>Contacto</h1>
+    return (
+        <div>
+             <h1 className='main-contacto'>Contacto</h1>
+                <hr className='main-separator'/>
+            <div className='main-contacto-1'>
+               
+                <div className=''>
+                    <div className=''>
+                    <div className='contacto-part'>
+                        <h1>Dirección postal, teléfono y email</h1>
+                        <hr/>
+                        <h2>Centro de interpretación del Desierto de Bolarque</h2>
+                        <h2>C/ San Roque, 6. Sayatón – Guadalajara (España)</h2>
+                        <h2>Teléfono: 949 38 94 07</h2>    
+                        <h2>Fax: 949 38 94 07</h2>
+                        <h2>Dirección de contacto general – desiertodebolarque@gmail.com</h2>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Contacto;
